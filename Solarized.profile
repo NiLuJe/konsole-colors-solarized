f1d02f7393d9969c744c6b1335267c8e8943c130
Ivan Shapovalov <intelfx100@gmail.com>
2013-04-15 11:44:06 +0400

    Add "Solarized" profile to be installed automatically.

---
+++ Solarized.profile
@@ -0,0 +1,10 @@
+[Appearance]
+ColorScheme=Solarized Dark
+
+[Cursor Options]
+CustomCursorColor=88,110,117
+UseCustomCursorColor=false
+
+[General]
+Name=Solarized
+Parent=FALLBACK/
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-kde_path="$(kde4-config --localprefix)"
+kde_path="$( kde4-config --localprefix )"
 
 for file in *Dark*; do
 	./solarized_convert.sh "$file" > "${file/Dark/Light}"
 done
 
-install -Dm644 *.colorscheme "${kde_path}/share/apps/konsole"
+rm -vf "${kde_path}/share/apps/konsole"/*Solarized*
+install -Dm644 *.colorscheme Solarized.profile "${kde_path}/share/apps/konsole"
 
 rm *Light*
