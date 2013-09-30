c2c8f937fbd257a274f4cbda4159e5209014c0b7
Ivan Shapovalov <intelfx100@gmail.com>
2013-09-30 10:14:25 +0400

    install.sh: correctly create the target directory.

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-kde_path="$( kde4-config --localprefix )"
+kde_path="$(kde4-config --localprefix)"
 
 for file in *Dark*; do
 	./solarized_convert.sh "$file" > "${file/Dark/Light}"
 done
 
+install -d "${kde_path}/share/apps/konsole"
 rm -vf "${kde_path}/share/apps/konsole"/*Solarized*
-install -Dm644 *.colorscheme Solarized.profile "${kde_path}/share/apps/konsole"
+install -m644 *.colorscheme Solarized.profile "${kde_path}/share/apps/konsole"
 
 rm *Light*
