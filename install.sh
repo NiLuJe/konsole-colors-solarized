fc459658a7e168ce64e2c6da39faaed7b4944994
Ivan Shapovalov <intelfx100@gmail.com>
2014-04-03 14:32:55 +0400

    install.sh: add a fallback for absent kde4-config.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 kde_path="$(kde4-config --localprefix)"
+if [[ -z "$kde_path" ]]; then kde_path="$HOME/.kde4"; fi
 
 for file in *Dark*; do
 	./solarized_convert.sh "$file" > "${file/Dark/Light}"
